ef362525d6f2a166b6f34f116f070c463c921b3f
Hossain <moffassal.hossain@siemens.com>
2017-01-27 14:39:44 +0100

    Fixed package.json and added more functionality

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ var server = restify.createServer();
 server.use(restify.queryParser());
 
 var d = new Date();
-var curStation = d.getMinutes();
+
+var firstTimeGeneratingCars;
 
 var trainArrivalData = [];
+var trainCarData = [];
 
 // const http = require('http');
 const port = 3000;
@@ -28,41 +30,78 @@ function random(low, high) {
   return Math.round(Math.random() * (high - low) + low);
 }
 
-function getRandomTrainArrivalData(type) {
+function getRandomTrainArrivalData(trainId) {
+ // ask implementation becuase this doesnt work...
+ return Math.floor(random(1, 21));
+
+  if (trainId == -1) {
+    for(i = 0; i <= 9; i++){
+      var randArrival = random(0, 21);
+      trainArrivalData.push(randArrival);
+    }
+  } else {
+    console.log(trainArrivalData.length + " " + trainId);
+    console.log("returning: " + trainArrivalData[trainArrivalData.length-trainId]);
+    return trainArrivalData[trainArrivalData.length-trainId];
+  }
+}
+
+function getRandomCarData(trainId) {
   var minutes = d.getMinutes();
 
-  if(minutes % 2 == 0 || type == 0) {
-      var randArrival = random(0, 21);
-      trainArrivalData.push()
-      return random(0, 21);
+  if(minutes % 2 == 0 && firstTimeGeneratingCars) {
+      var randCars = generateCarts();
+      trainCarData = [];
+      trainCarData.push(randCars);
+      console.log("%2=0: " + randCars);
+      firstTimeGeneratingCars = false;
+      return randCars;
+  } else if (minutes % 2 == 0 && !firstTimeGeneratingCars) {
+      console.log("else % 2: " + trainCarData[trainArrivalData.length - trainId]);
+      return trainCarData[trainArrivalData.length - trainId];
   } else {
-    return trainArrivalData;
+    console.log("else: " + trainCarData[trainArrivalData.length - trainId]);
+    firstTimeGeneratingCars = true;
+    return trainCarData[trainArrivalData.length - trainId];
   }
 }
 
 function getStationInfo(stationName, type){
+  if(trainArrivalData.length == 0){
+    getRandomTrainArrivalData(-1);
+  }
   if (stationName == "Ballerup") {
-    return [createTrain("Klampenborg", "C", getRandomTrainArrivalData(type), generateCarts(), 2),
-    createTrain("Frederikssund", "C", getRandomTrainArrivalData(type), generateCarts(), 1)];
+    return [
+      createTrain("Klampenborg", "C", getRandomTrainArrivalData(0), getRandomCarData(0), 2),
+      createTrain("Frederikssund", "C", getRandomTrainArrivalData(1), getRandomCarData(1), 1)
+    ];
   }
   else if (stationName == "Flintholm") {
-    return [createTrain("Hellerup", "F", getRandomTrainArrivalData(type), generateCarts(), 2),
-    createTrain("Ny Ellebjerg", "F", getRandomTrainArrivalData(type), generateCarts(), 1)];
+    return [
+      createTrain("Hellerup", "F", getRandomTrainArrivalData(2), getRandomCarData(2), 2),
+      createTrain("Ny Ellebjerg", "F", getRandomTrainArrivalData(3), getRandomCarData(3), 1)
+    ];
   }
   else if (stationName == "Jersie") {
-    return [createTrain("Køge", "E", getRandomTrainArrivalData(type), generateCarts(), 2),
-    createTrain("Hillerød", "E", getRandomTrainArrivalData(type), generateCarts(), 1)];
+    return [
+      createTrain("Køge", "B", getRandomTrainArrivalData(4), getRandomCarData(4), 2),
+      createTrain("Hillerød", "A", getRandomTrainArrivalData(5), getRandomCarData(5), 1)
+    ];
   }
   else if (stationName == "Ølstykke") {
-    return [createTrain("Frederikssund", "C", getRandomTrainArrivalData(type), generateCarts(), 2),
-    createTrain("Klampenborg", "C", getRandomTrainArrivalData(type), generateCarts(), 1)];
+    return [
+      createTrain("Frederikssund", "E", getRandomTrainArrivalData(6), getRandomCarData(6), 2),
+      createTrain("Klampenborg", "H", getRandomTrainArrivalData(7), getRandomCarData(7), 1)
+    ];
   }
   else if (stationName == "London") {
-    return [createTrain("Hogsmeade", "Q", getRandomTrainArrivalData(type), generateCarts(), 9.75),
-    createTrain("Ny Klampenborg", "Q", getRandomTrainArrivalData(type), generateCarts(), 9.75)];
+    return [
+      createTrain("Hogsmeade", "A", getRandomTrainArrivalData(8), getRandomCarData(8), 9.75),
+      createTrain("Ny Klampenborg", "A", getRandomTrainArrivalData(9), getRandomCarData(9), 9.75)
+    ];
   }
   else {
-    return createTrain("Unknown", "Z", 0, [0,0,0], 0);
+    return [createTrain("Unknown", "Z", 0, [0,0,0], 0)];
   }
 }
 
@@ -77,6 +116,7 @@ function generateCarts() {
   for(i = 0; i < numOfCarts; i++){
     var randomWeight = random(0, 8000);
     result += Math.round(randomWeight / maxAllowPersonsPerCar)
+    if(result > 1) result = 1;
     if(i+1 < numOfCarts)
       result += ",";
   }
